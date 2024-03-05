4c3aaf6efefcd3bd97477842f52d3bdcebf517d4
Shishir-Ahmed <shishirahmed790@gmail.com>
2024-03-06 00:19:22 +0600

    created left side nav initially

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,18 @@
+import LeftSideNav from "../Shared/LeftSideNav/LeftSideNav";
+
 const Home = () => {
-  return <div>this is our home page</div>;
+  return (
+    <div>
+      <div className="container mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+          <div className="my-28">
+            <LeftSideNav></LeftSideNav>
+          </div>
+          {/* something is coming here */}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
