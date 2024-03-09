9978fcff198ea7f8a2c513159fa7a4bc363d5835
raiyanrimon <raiyan1615@sttudent.nstu.edu.bd>
2024-03-09 12:53:11 +0600

    welcome and work strands images are changed

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -3,28 +3,17 @@ import React from "react";
 const Welcome: React.FC = () => {
   return (
     <div className="bg-teal-900 text-white  flex flex-col justify-center items-center p-8 md:p-16">
-      <div className="text-center max-w-2xl mx-auto">
+      <div className="text-center max-w-3xl mx-auto">
         <h1 className="lg:text-6xl text-4xl font-extrabold mb-4">
           Welcome to Daar-Un-Noor Sveriges
         </h1>
-        <p className="lg:text-xl text-lg mb-6">
-          Embracing spiritual, moral, and social renewal in the Muslim
-          community.
-        </p>
         <p className="lg:text-lg text-base mb-6">
-          Our programs aim to enlighten minds about the profound message of
-          Islam and its enduring values. We are dedicated to providing education
-          that empowers Muslims to actively engage, integrate into society, and
-          supports Swedish Muslims in fulfilling their religious duties.
-        </p>
-        <p className="lg:text-lg text-base mb-6">
-          As revealed in the Quran (2:270), those who contribute to Allah’s
-          cause will be rewarded without fear or grief.
-        </p>
-        <p className="lg:text-lg text-base mb-6">
-          Join us in nurturing a united and flourishing community. If you're
-          intrigued by our educational program on Islam, kindly share your name,
-          address, postcode, email, and mobile/telephone number.
+          Daar Un Noor Sweden is a grassroots social organization set up to
+          encourage and facilitate Muslims to be active participants in society
+          to benefit all members of humanity. It organizes members to seek the
+          pleasure of Allah through worship of Him and service to humanity. Daar
+          Un Noor Sweden's aims to bring about spiritual, moral, and social
+          renewal through the values enshrined in the Islamic faith.
         </p>
       </div>
     </div>
