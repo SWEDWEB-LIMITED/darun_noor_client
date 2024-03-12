451c885e3fe84081b8415b060f784e6559e064c3
raiyanrimon <raiyan1615@sttudent.nstu.edu.bd>
2024-03-13 00:58:42 +0600

    campaign card updated

--- src/components/Campaign/Campaign.tsx
+++ src/components/Campaign/Campaign.tsx
@@ -9,8 +9,8 @@ interface CampaignProps {
 const Campaign: React.FC<CampaignProps> = ({ image, title, time }) => {
   return (
     <div className="max-w-xl mx-auto p-6 bg-white rounded-md shadow-md mt-8">
-      <div className="overflow-hidden h-64 rounded-md hover:scale-110 transition-transform">
-        <img src={image} alt={title} className="object-cover" />
+      <div className="overflow-hidden md:h-64 rounded-md hover:scale-110 transition-transform">
+        <img src={image} alt={title} className="object-contain " />
       </div>
       <h3 className="text-xl font-bold mb-2">{title}</h3>
       <p className="text-gray-600">{time}</p>
